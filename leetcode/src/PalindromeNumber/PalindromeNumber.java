1a902cf2405709c2814dffe5d441eb90d2591417
PhilyaJke <borozdinfilipp@gmail.com>
2023-05-11 19:40:54 +0300

    PalindromeNumber

---
+++ leetcode/src/PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,18 @@
+package PalindromeNumber;
+
+class PalindromeNumber{
+    public static boolean isPalindrome(int x){
+        int input = x;
+        int num = 0;
+        if(input < 0){
+            return false;
+        }else {
+            while (input != 0) {
+                num *= 10;
+                num += input % 10;
+                input /= 10;
+            }
+            return x == num;
+        }
+    }
+}
