0a7fb691da7ab06780564f225fb1cca572e6d03e
PhilyaJke <borozdinfilipp@gmail.com>
2023-05-11 20:19:25 +0300

    SumInLoop

---
+++ codeabbey/src/main/SumInLoop.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.Scanner;
+
+public class SumInLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String[] arr = str.split(" ");
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result+=Integer.parseInt(arr[i]);
+        }
+        System.out.println(result);
+    }
+}
