d97c1b4680d20fec97a8226e6ba0774e8af5035f
PhilyaJke <borozdinfilipp@gmail.com>
2023-05-11 20:03:01 +0300

    SumInLoop&SumAB

---
+++ codeabbey/src/main/SumAB/SumAB.java
@@ -0,0 +1,13 @@
+package main.SumAB;
+
+import java.util.Scanner;
+
+public class SumAB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
