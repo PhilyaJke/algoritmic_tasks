0477c95b547b6f270a38ccf0e36687cef1bd215d
PhilyaJke <borozdinfilipp@gmail.com>
2023-05-11 20:17:38 +0300

    SumAB

---
+++ codeabbey/src/main/ABSum.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Scanner;
+
+public class ABSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
