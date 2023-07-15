2cd92637138571ec33f6bf62cea91b242b9ff5f5
PhilyaJke <borozdinfilipp@gmail.com>
2023-07-16 00:44:22 +0300

    ValidParentheses.java

---
+++ leetcode/src/ValidParentheses.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "(([]){})";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
+
