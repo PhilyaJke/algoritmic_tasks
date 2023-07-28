32aea7003b075615f6dcd19dacedb517c9e0b94c
PhilyaJke <borozdinfilipp@gmail.com>
2023-07-28 19:19:22 +0300

    FirstBadVersion.java

--- leetcode/src/FirstBadVersion.java
+++ leetcode/src/FirstBadVersion.java
@@ -5,12 +5,16 @@ public class FirstBadVersion{
     public int firstBadVersion(int n) {
         int index = -1;
         while (n!=0){
-            if(isBadVersion(n)==false){
+            if(isBadVersion(n,3)==false){
                 index = n;
                 break;
             }
             n--;
         }
         return index;
+    }
+
+    public static boolean isBadVersion(int num, int j){
+        return num==j;
     }
 }
