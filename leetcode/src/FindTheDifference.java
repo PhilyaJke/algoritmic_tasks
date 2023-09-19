780694d19102b5d5695f9ef004055aa995e9c4a8
PhilyaJke <borozdinfilipp@gmail.com>
2023-09-19 22:34:30 +0300

    FindTheDifference

---
+++ leetcode/src/FindTheDifference.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class FindTheDifference {
+    public static void main(String[] args) {
+        System.out.println(findTheDifference("ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor", "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj"));
+    }
+    public static char findTheDifference(String s, String t) {
+        if(s.length()==0){
+            return t.charAt(0);
+        }
+        HashMap<Character, Integer> hashMap1 = new HashMap<>();
+        HashMap<Character, Integer> hashMap2 = new HashMap<>();
+        char answer = t.charAt(t.length()-1);
+        for (int i = 0; i < s.length(); i++) {
+            char val = s.charAt(i);
+            if(!hashMap1.containsKey(val)){
+                hashMap1.put(val, 1);
+            }else{
+                int count = hashMap1.get(val);
+                hashMap1.put(val, ++count);
+            }
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char val = t.charAt(i);
+            if(!hashMap2.containsKey(val)){
+                hashMap2.put(val, 1);
+            }else{
+                int count = hashMap2.get(val);
+                hashMap2.put(val, ++count);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char val = t.charAt(i);
+            if(!hashMap1.containsKey(val)){
+                answer = val;
+            }else{
+                int num1 = hashMap1.get(val);
+                int num2 = hashMap2.get(val);
+                if (num1 < num2) {
+                    answer = val;
+                }
+            }
+        }
+        return answer;
+    }
+}
